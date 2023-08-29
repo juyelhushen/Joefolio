085b7992289b28ef6093b3a7cc02a9a966b41ac3
Juyel <juyelhushen@gmail.com>
2023-08-29 15:50:31 +0530

    snackbar and email js service added.

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,35 +1,49 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Globalconstant } from 'src/app/shared/global-constant';
+import emailjs from '@emailjs/browser';
+import { Contact } from 'src/app/models/contact';
+import { environment } from 'src/environments/environment';
+import { SnackbarService } from 'src/app/services/snackbar.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+
+  responseMessage: any;
+  key = environment.emailJSKey;
+
   contactForm: any = FormGroup;
 
-  constructor(private fb: FormBuilder) {}
+  constructor(private fb: FormBuilder,
+    private snackbar:SnackbarService) {}
 
   ngOnInit(): void {
     this.contactForm = this.fb.group({
-      name: [
-        null,
-        [Validators.pattern(Globalconstant.nameRegex)]
-      ],
-      email: [
-        null,
-        [Validators.pattern(Globalconstant.emailRegex)]
-      ],
-      number: [
-        null,
-        [Validators.pattern(Globalconstant.contactNumberRegex),Validators.maxLength(10)]
-      ],
-      message: [
-        null,
-        [Validators.minLength(20)]
-      ]
+      name: [null, [Validators.pattern(Globalconstant.nameRegex)]],
+      email: [null, [Validators.pattern(Globalconstant.emailRegex)]],
+      subject: [null, [Validators.minLength(10)]],
+      message: [null, [Validators.minLength(20)]],
     });
+  }
+
+
+
+  async onSubmit() {
+    emailjs.init(this.key);
+    const formData = this.contactForm.value;
+    let response = await emailjs.send('service_groma9h', 'template_94r6juq', {
+      from_name: formData.name,
+      to_name: 'Juyel',
+      from_email: formData.email,
+      subject: formData.subject,
+      message: formData.message,
+    });
+    this.responseMessage = 'Thank you .';
+    this.snackbar.openSnackBar(this.responseMessage,'')
+    this.contactForm.reset();
   }
 }
